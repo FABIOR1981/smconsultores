53b9513436dc502cdddd50adfb23427ea8deddc4
Roberto Elola, Jorge Fabio <fabior1981@gmail.com>
2026-06-14 01:15:21 -0300

    999

--- logos.js
+++ logos.js
@@ -2,6 +2,7 @@
 const clientesComex = [
     { nombre: "Aramaycia", logo: "1_logo-aramaycia.png" },
     { nombre: "Santa Rosa", logo: "2_logo-ibarra.png" },
-    { nombre: "Ibarra Comex", logo: "3_logo-santarosa.png" },
-    { nombre: "Liguori Dendi", logo: "4_logo-liguori.png" }
+    
+    { nombre: "alguno1", logo: "5_logistica-comex.jpg" },
+    { nombre: "alguno2", logo: "6_auditoria-6_soporte.jpg" }
 ];
