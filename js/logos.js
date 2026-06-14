a0812bc8795108c1f300521500a003e36ae50f2d
Roberto Elola, Jorge Fabio <fabior1981@gmail.com>
2026-06-14 13:50:26 -0300

    1111

--- js/logos.js
+++ js/logos.js
@@ -3,5 +3,8 @@ const clientesComex = [
     { nombre: "Aramaycia", logo: "1_logo-aramaycia.png" },
     { nombre: "Ibarra Comex", logo: "2_logo-ibarra.png" },
     { nombre: "Santa Rosa", logo: "3_logo-santarosa.png" },
-    { nombre: "Liguori Dendi", logo: "4_logo-liguori.png" }
+    { nombre: "Liguori Dendi", logo: "4_logo-liguori.png" },
+    { nombre: "ACE", logo: " 5_logo-ace.png" }
+
+   
 ];
